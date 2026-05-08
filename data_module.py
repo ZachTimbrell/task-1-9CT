a782bc7298737ce4c315eeff6dfc6e9e4acc5a2a
ZachTimbrell <zachary.timbrell@education.nsw.gov.au>
2026-05-08 19:34:58 +1000

    main

--- data_module.py
+++ data_module.py
@@ -1,12 +1,21 @@
 import pandas as pd
 def display_dataset_preview_GSW():
     display_dataset_preview_GSW = pd.DataFrame([
-        le
+    ['0', 'PG', '6-2', '190', 'May 22, 1968']
+    ['25', 'PG',	'6-3',	'175',	'September 27, 1965']
+    ['9', 'PG',	'6-6',	'185',	'January 20, 1964']
+    ['23', 'SG',	'6-6',	'198',	'February 17, 1963']
+    ['30', 'SF', '6-6',	'220',	'June 19, 1968']
+    ['7', 'SF',	'6-10',	'192',	'September 18, 1968']
+    ['33', 'SF',	'6-8',	'210',	'September 25, 1965']
+    ['35', 'PF',	'6-8',	'255',	'June 12, 1973']
+    ['91', 'PF',	'6-7',	'210',	'May 13, 1961']
+    ['22', 'PF',	'6-11',	'230',	'May 16, 1964']
+    ['8', 'PF',	'6-9',	'248',	'April 6, 1972']
+    ['53', 'C',	'7-1',	'252',	'November 22, 1955']
+    ['54', 'C',	'6-10',	'240	January 27, 1964']
+    ['13', 'C',	'7-2',	'265',	'January 19, 1969']
+    ['34', 'C',	'7-0',	'245',	'April 26, 1963']  
+    ], columns = ['Randy Brown', 'Steve Kerr', 'Ron Haprer', 'Michael Jordan', 'Jud Buechler', 'Toni Kukoč', 'Scottie Pippen', 'Jasen Caffey', 'Dennis Rodman', 'John Salley', 'Dickey Simpkins', 'James Edwards', 'Jack Haley', 'luc Longley','Bill Wellington'])
 
-
-
-
-
-
-
-    ])
+print(display_dataset_preview_GSW)
